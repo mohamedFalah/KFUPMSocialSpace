40ad98b2ea7c2d9d0f08f012f694639251616861
JabbarAlQassab <Jabbar.AlQassab@gmail.com>
2018-11-24 01:12:51 +0300

    Testing Firebase for sign up functionality

---
+++ signupTest/src/main/java/com/example/signuptest/SignUp.java
@@ -0,0 +1,27 @@
+package com.example.signuptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        Button signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
