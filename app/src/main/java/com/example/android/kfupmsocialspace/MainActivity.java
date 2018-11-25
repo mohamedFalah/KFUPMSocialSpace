37ea781fbcc0dbab0c05bb137d6f6884104723e3
Ridha Alawami <xtok96@gmail.com>
2018-11-25 19:21:42 +0300

    Clickable bottom navigation remaining to uncheck if the other is checked

--- app/src/main/java/com/example/android/kfupmsocialspace/MainActivity.java
+++ app/src/main/java/com/example/android/kfupmsocialspace/MainActivity.java
@@ -17,24 +17,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout drawer;
 
-    //Bottom Navigation bar Listener
-    private BottomNavigationView.OnNavigationItemSelectedListener botNavListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-
-                    switch (item.getItemId()) {
-                        case (R.id.navigation_market):
-                            selectedFragment = new MarketFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-                    return true;
-                }
-            };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,16 +29,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //Here is for the fragments after creating them to listen to the button clicks
         drawer = findViewById(R.id.drawer_layout);
 
-
-        NavigationView navigationview = findViewById(R.id.nav_view);
-        //https://www.youtube.com/watch?v=bjYstsO1PgI 5:00 we implements the interface
-        navigationview.setNavigationItemSelectedListener(this);
-
-        //adding the Listener to the bottom navigation bar
-        BottomNavigationView bottomnNav = findViewById(R.id.bottom_navigation);
-        bottomnNav.setOnNavigationItemSelectedListener(botNavListener);
-
-
         /*
         This is to show the action bar sliding. before that we add 2 strings in strings.xml
         the navigation_drawer_open And navigation_drawer_close for Blind people so it can be read by the device.
@@ -65,39 +37,83 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
 
+        NavigationView navigationview = findViewById(R.id.nav_view);
+        //https://www.youtube.com/watch?v=bjYstsO1PgI 5:00 we implements the interface
+        navigationview.setNavigationItemSelectedListener(this);
+
+        //adding the Listener to the bottom navigation bar
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(botNavListener);
 
         //This is the first things you see when you open the app before clicking on any activity from the toolbar
         // https://www.youtube.com/watch?v=bjYstsO1PgI 9:20 when device rotates we don't call the method again
-        if (savedInstanceState == null)
+        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new ProfileFragment()).commit();
-        navigationview.setCheckedItem(R.id.nav_profile);
+                    new NewsFragment()).commit();
+            bottomNav.setSelectedItemId(R.id.navigation_news);
+        }
 
         //just rotates the three line hamburger symbol with the device
         toggle.syncState();
     }
+    //Bottom Navigation bar Listener
+    private BottomNavigationView.OnNavigationItemSelectedListener botNavListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case (R.id.navigation_chats):
+                            selectedFragment = new ChatsFragment();
+                            break;
+                        case (R.id.navigation_blogs):
+                            selectedFragment = new BlogsFragment();
+                            break;
+                        case (R.id.navigation_news):
+                            selectedFragment = new NewsFragment();
+                            break;
+                        case (R.id.navigation_roommate):
+                            selectedFragment = new RoommateFragment();
+                            break;
+                        case (R.id.navigation_market):
+                            selectedFragment = new MarketFragment();
+                            break;
+                    }
+                    if(selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+                        if(item.isChecked()){
+
+                        }
+                    }
+
+                    return true;
+                }
+            };
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
         switch (item.getItemId()) {
-            case R.id.nav_profile:
-                //getSupportFragmentManager shows the fragment in the framelayout
+            case (R.id.nav_profile):
+                selectedFragment = new ProfileFragment();
                 break;
-
-            case R.id.nav_utilities:
-                //getSupportFragmentManager shows the fragment in the framelayout
+            case (R.id.nav_utilities):
+                selectedFragment = new UtilitiesFragment();
                 break;
-
-            case R.id.nav_files:
-                //getSupportFragmentManager shows the fragment in the framelayout
+            case (R.id.nav_files):
+                selectedFragment = new FilesFragment();
                 break;
-
-            case R.id.nav_logout:
-                Toast.makeText(this, "Replace with Log out action", Toast.LENGTH_SHORT).show();
+            case (R.id.nav_logout):
+                item.setChecked(false);
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
                 break;
         }
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new UtilitiesFragment()).commit();
+        if(selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+        }
 
         //After clicking an item we need to close the drawer so we can get to the page and see it
         drawer.closeDrawer(GravityCompat.START);
