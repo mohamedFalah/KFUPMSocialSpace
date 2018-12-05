ed6cd60df2d234216129e306114d6cd2850cf74e
MohamedFalah <mohamshualh@gmail.com>
2018-12-05 19:39:06 +0300

    message class and message addapter plus the layout for sender message plus changing the chatactivitychatfragment to activity

--- app/src/main/java/com/example/android/kfupmsocialspace/ChatActivity.java
+++ app/src/main/java/com/example/android/kfupmsocialspace/ChatActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.kfupmsocialspace;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.widget.RecyclerView;
@@ -39,14 +43,16 @@ public class ChatActivity extends AppCompatActivity {
     private EditText chatMsgField;
     private RecyclerView mMessagesList;
 
+
     public BottomNavigationView.OnNavigationItemSelectedListener chatBotNavListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
+                    Activity activity = null;
                     switch (item.getItemId()) {
                         case (R.id.chat):
-                            selectedFragment = new ChatActivityChatFragment();
+                            activity = new ChatActivityChatFragment();
                             break;
                         case (R.id.chat_files):
                             selectedFragment = new ChatFilesFragment();
@@ -59,9 +65,17 @@ public class ChatActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.chat_fragment_container,
                                 selectedFragment).commit();
                     }
+                    if (activity != null) {
+                        Intent intent = new Intent(getApplicationContext(), ChatActivityChatFragment.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+
+
                     return true;
                 }
             };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +83,6 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
 
-        mMessagesList = findViewById(R.id.messages_list);
 
 
         BottomNavigationViewEx chatBottomNav = findViewById(R.id.chat_bottom_navigation);
@@ -83,61 +96,8 @@ public class ChatActivity extends AppCompatActivity {
         }
 
 
-        /*
-
-
-        chatAttachFileBtn =  findViewById(R.id.attach_file);
-        chatSendBtn = findViewById(R.id.send_message);
-        chatMsgField =  findViewById(R.id.message_string);
-
-        chatSendBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SendMsg();
-
-            }
-        });
-
-        */
-
-
 
     }
-
-
-
-    private void SendMsg() {
-
-        String msg = chatMsgField.getText().toString();
-
-        if(!TextUtils.isEmpty(msg)){
-            DatabaseReference push = dbRef.child("section1").push();
-            String push_Id = push.getKey();
-
-            Map msgMap = new HashMap();
-            msgMap.put("id", currentUserId);
-            msgMap.put("msg" , msg);
-
-            Map map2 = new HashMap();
-            map2.put("section2/"+push_Id,msgMap);
-
-            dbRef.updateChildren(map2, new DatabaseReference.CompletionListener() {
-                @Override
-                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
-                    if(databaseError!= null){
-                        Log.d("CHAT_LOG", databaseError.getMessage().toString());
-                    }
-                }
-            });
-
-            chatMsgField.setText("");
-        }
-
-
-    }
-
-
 
 
 
--- app/src/main/java/com/example/android/kfupmsocialspace/ChatActivityChatFragment.java
+++ app/src/main/java/com/example/android/kfupmsocialspace/ChatActivityChatFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,14 +20,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ChatActivityChatFragment extends Fragment {
+public class ChatActivityChatFragment extends AppCompatActivity {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference dbRef = database.getReference("Message");
@@ -33,24 +40,102 @@ public class ChatActivityChatFragment extends Fragment {
 
 
 
+    private final List<Message> messageList = new ArrayList<>();
+
+    private LinearLayoutManager linearLayoutManager;
+
+    private MessageAdapter messageAdapter;
+
+    private RecyclerView userMessagesList;
+
 
     ImageButton chatAttachFileBtn;
     ImageButton chatSendBtn ;
     EditText chatMsgField ;
 
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_chat_fragment);
+
+
+        chatAttachFileBtn =  findViewById(R.id.attach_file);
+        chatSendBtn = findViewById(R.id.send_message);
+        chatMsgField =  findViewById(R.id.message_string);
+
+
+        messageAdapter = new MessageAdapter(messageList);
+        userMessagesList = findViewById(R.id.messages_list);
+        linearLayoutManager = new LinearLayoutManager(this);
+        userMessagesList.setLayoutManager(linearLayoutManager);
+        userMessagesList.setAdapter(messageAdapter);
+
+
+
+        chatAttachFileBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "This is", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        chatSendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                sendMsg();
+
+
+            }
+
+
+
+
+        });
+
+        chatMsgField.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Do it.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
+
+
+
+/*
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //inflates sets or shows the fragment selected in the first param
-        return inflater.inflate(R.layout.activity_chat_chat_fragment, container, false);
+        View view =  inflater.inflate(R.layout.activity_chat_chat_fragment, container, false);
+
+        messageAdapter = new MessageAdapter(messageList);
+        userMessagesList = view.findViewById(R.id.messages_list);
+        linearLayoutManager = new LinearLayoutManager(this.getContext());
+        userMessagesList.setLayoutManager(linearLayoutManager);
+        userMessagesList.setAdapter(messageAdapter);
+
+        return view;
+
     }
 
+    */
+/*
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
          chatAttachFileBtn =  view.findViewById(R.id.attach_file);
          chatSendBtn = view.findViewById(R.id.send_message);
          chatMsgField =  view.findViewById(R.id.message_string);
 
+
+
+
+
+        getMessages();
         chatAttachFileBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(getContext(), "This is", Toast.LENGTH_SHORT).show();
@@ -61,6 +146,7 @@ public class ChatActivityChatFragment extends Fragment {
             public void onClick(View v) {
 
                 sendMsg();
+
 
             }
 
@@ -75,6 +161,48 @@ public class ChatActivityChatFragment extends Fragment {
             }
         });
     }
+*/
+
+    @Override
+    protected void onStart(){
+
+        super.onStart();
+
+        dbRef.child("section2").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                Message message = dataSnapshot.getValue(Message.class);
+
+                messageList.add(message);
+
+                messageAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
 
 
 
@@ -87,8 +215,8 @@ public class ChatActivityChatFragment extends Fragment {
             String push_Id = push.getKey();
 
             Map msgMap = new HashMap();
-            msgMap.put("id", currentUserId);
-            msgMap.put("msg" , msg);
+            msgMap.put("senderID", currentUserId);
+            msgMap.put("Text" , msg);
 
             Map map2 = new HashMap();
             map2.put("section2/"+push_Id,msgMap);
