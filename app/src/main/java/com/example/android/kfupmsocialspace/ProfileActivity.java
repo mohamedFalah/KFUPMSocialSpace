1eb0eec4a1d012729257eaaac727781216205fa8
Ridha Alawami <xtok96@gmail.com>
2019-01-02 18:00:19 +0300

    Added dummy edit profile activity
    Signed-off-by: Ridha Alawami <xtok96@gmail.com>

--- app/src/main/java/com/example/android/kfupmsocialspace/ProfileActivity.java
+++ app/src/main/java/com/example/android/kfupmsocialspace/ProfileActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.kfupmsocialspace;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button button = findViewById(R.id.edit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -17,4 +28,5 @@ public class ProfileActivity extends AppCompatActivity {
         super.onBackPressed();
         NavUtils.navigateUpFromSameTask(this);
     }
+
 }
